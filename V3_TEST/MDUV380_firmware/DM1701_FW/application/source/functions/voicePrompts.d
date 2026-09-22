b68c4d6d87f8928640e4da49c8a2d6c90c84f215
RichardL007 <132619629+RichardL007@users.noreply.github.com>
2026-09-22 14:22:34 +0100

    Remove untracked firmware build files

--- V3_TEST/MDUV380_firmware/DM1701_FW/application/source/functions/voicePrompts.d
+++
@@ -1,4 +0,0 @@
-application/source/functions/voicePrompts.o: \
- ../application/source/functions/voicePrompts.c \
- ../application/include/functions/voicePrompts.h
-../application/include/functions/voicePrompts.h:
